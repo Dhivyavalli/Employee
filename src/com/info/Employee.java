b92147fa4aaf6925cf7f812d95552d3f2153b086
Dhivyavalli <abidivi356@gmail.com>
2025-02-13 18:01:46 +0530

    Message

---
+++ src/com/info/Employee.java
@@ -0,0 +1,25 @@
+package com.info;
+
+public class Employee {
+	public static void main(String[] args) {
+		byte empId = 123;
+		System.out.println("Employee ID = "+empId);
+		String name = "Dhivya";
+		System.out.println("Employee Name = "+name);
+		String mailId = "abidivi356@gmail.com";
+		System.out.println("Employee Mail ID = "+mailId);
+		long phoneNo = 1234567890;
+		System.out.println("Employee Phone Number"+phoneNo);
+		int salary = 10000;
+		System.out.println("Employee Salary"+salary);
+		String gender = "Female";
+		System.out.println("Employee Gender = "+gender);
+		String city = "Chennai";
+		System.out.println("Employee City = "+city);
+		boolean result = true;
+		System.out.println("All above Information are "+result);
+		
+		
+	}
+}
+
